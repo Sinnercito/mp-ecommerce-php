5c8c62fe6fda6f7fdfa2a74a3f9a6100f6d51586
Tincho <fontanamartin@gmail.com>
2020-06-11 17:35:52 -0300

    Ultimo

---
+++ cancelado.php
@@ -0,0 +1,12 @@
+<?php
+echo (" Algo salio mal... ");
+
+
+echo (" Volvamos al sitio para intenetrlo otra vez!! ")
+
+
+
+?>
+
+<br>
+<a href="https://sinnercito-mp-commerce-php.herokuapp.com/">A comprar MAS.. Con todos los beneficios de MP!!!!</a>
--- pendiente.php
+++ pendiente.php
@@ -1,4 +1,7 @@
 <?php
-echo ("el putoooo");
+echo ("Esto esta pendiente de pago__:::");
 echo ($_GET["collector_id"]);
 ?>
+
+<br>
+<a href="https://sinnercito-mp-commerce-php.herokuapp.com/">A comprar MAS.. Con todos los beneficios de MP!!!!</a>
