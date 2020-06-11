d34b908a12d3123ef9d33b867de41c0841b6273b
Tincho <fontanamartin@gmail.com>
2020-06-11 15:06:48 -0300

    24

--- yaestuyo.php
+++ yaestuyo.php
@@ -1,9 +1,12 @@
 <?php
 echo("El pago fue tomado y guardado con exito!! falta poco para disfrutar de lo que ya es tuyo!! ");
 echo("</br>");
-$_GET["payment_method_id"];
-$_GET["external_reference"];
-$_GET["collection_id"];
-
-
+echo $_GET["payment_method_id"];
+echo $_GET["external_reference"];
+echo $_GET["collection_id"];
 ?>
+
+
+<a href="https://sinnercito-mp-commerce-php.herokuapp.com/">A comprar MAS.. Con todos los beneficios de MP!!!!</a>
+
+
