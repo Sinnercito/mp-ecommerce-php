7e7a859af0ff727152f162fc913f10708be856ac
Tincho <fontanamartin@gmail.com>
2020-06-11 14:24:32 -0300

    lo otro

--- detail.php
+++ detail.php
@@ -55,6 +55,8 @@ $preference->payment_methods = array(
 
 $preference->external_reference="fontanamartin@gmail.com";
 
+$preference->id="dev_24c65fb163bf11ea96500242ac130004";
+
 $preference->back_urls = array(
     "success" => "https://sinnercito-mp-commerce-php.herokuapp.com/yaestuyo",
     "failure" => "https://sinnercito-mp-commerce-php.herokuapp.com/canceldo",
