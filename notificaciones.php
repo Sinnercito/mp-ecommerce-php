688dbec40bf7a4771f20bffbdfb6b94688cd4006
Martin Oscar Fontana <mfontana@GLP.GOV.AR>
2020-06-12 10:04:39 -0300

    445

--- notificaciones.php
+++ notificaciones.php
@@ -4,7 +4,11 @@
    
   
   MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+  
 
+  
+  
+  
   
   switch($_POST["type"]) {
         case "payment":
@@ -22,6 +26,9 @@
             break;
         case "invoice":
             $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+            case "test":
+                echo ("la puta");
             break;
     }
 
