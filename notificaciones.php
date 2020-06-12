35d27258fb7d222875d586172fb59083a81f587b
Martin Oscar Fontana <mfontana@GLP.GOV.AR>
2020-06-12 10:33:11 -0300

    4554+

--- notificaciones.php
+++ notificaciones.php
@@ -2,10 +2,7 @@
   
   require __DIR__ .  '/vendor/autoload.php';
    
-  
   MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-  
-  
   
   switch($_POST["type"]) {
         case "payment":
@@ -13,7 +10,7 @@
             header('Content-Type: application/json');
             echo json_encode(['HTTP/1.1 200 OK'], 200);
 
-            
+
             var_dump($payment);
             break;
         case "plan":
