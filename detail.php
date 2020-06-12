afec0b867945f94df6aa5133b5ec624d8dabbc07
Sinnercito <fontana_martin@hotmail.com>
2020-06-12 15:02:07 -0300

    11

--- detail.php
+++ detail.php
@@ -24,15 +24,15 @@ $item->description = "Dispositivo movil de Tienda e-commerce";
 $preference->items = array($item);
 
 $payer = new MercadoPago\Payer();
-  $payer->name = "Lalo";
-  $payer->surname = "Landa";
   $payer->first_name = "Lalo";
   $payer->last_name = "Landa";
   $payer->email = "test_user_63274575@testuser.com";
   $payer->date_created = "2018-06-02T12:58:41.425-04:00";
   $payer->phone = array(
     "area_code" => "11",
-    "number" => "22223333"
+    "number" => "22223333",
+    "extension" => "54"
+
   );
   
   $payer->identification = array(
@@ -43,7 +43,7 @@ $payer = new MercadoPago\Payer();
   $payer->address = array(
     "street_name" => "False",
     "street_number" => 123,
-    "zip_code" => "1111"
+    "zip_code" => 1111
   );
 $preference->payer = $payer;
 
